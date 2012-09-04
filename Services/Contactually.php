5835716dc8927d9226163ace5a226697e1a5e035
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-04 02:16:06 -0500

    shifted the data loading from the core class to the base class so all the resources can use it;

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -14,7 +14,16 @@ spl_autoload_register('Services_Contactually_autoload');
 class Services_Contactually
 {
     protected $cookie_path = '';
-    protected $sub_resources = array();
+    protected $sub_resources = array(
+                    'accounts' => 'Account',
+                    'buckets' => 'Bucket',
+                    'contact_histories' => 'ContactHistory',
+                    'contacts' => 'Contact',
+                    'followups' => 'Followup',
+                    'notes' => 'Note',
+                    'tasks' => 'Task',
+                    'users' => 'User'
+                );
 
     /*
      * Authentication derived from the docs: http://developers.contactually.com/
@@ -23,16 +32,6 @@ class Services_Contactually
     public function __construct($params)
     {
         $this->cookie_path = getcwd() . '/cookie.txt';
-        $this->sub_resources = array(
-            'accounts' => 'Account',
-            'buckets' => 'Bucket',
-            'contact_histories' => 'ContactHistory',
-            'contacts' => 'Contact',
-            'followups' => 'Followup',
-            'notes' => 'Note',
-            'tasks' => 'Task',
-            'users' => 'User'
-        );
 
         /*
          * I'm not a fan of this hack, but it seemed the most appropriate for the
@@ -44,10 +43,15 @@ class Services_Contactually
         }
         $auth_url = 'https://www.contactually.com/users/sign_in.json';
 
-        $this->_post($auth_url, $params);
+        $this->post($auth_url, $params);
+
+        foreach($this->sub_resources as $obj => $class) {
+            $classname = 'Services_Contactually_'.$class;
+            $this->$obj = new $classname($this);
+        }
     }
 
-    protected function _post($uri, $params = array())
+    public function post($uri, $params = array())
     {
         $curl_opts = array(
             CURLOPT_RETURNTRANSFER => true,
@@ -62,7 +66,7 @@ class Services_Contactually
         return $this->_execute($curl_opts);
     }
 
-    protected function _get($uri, $params = array())
+    public function get($uri, $params = array())
     {
         $uri .= '?'.http_build_query($params);
 
@@ -96,40 +100,5 @@ curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
         curl_close($connection);
 
         return json_decode($response);
-    }
-
-    /**
-     * This handles all the GET index actions for all of the resources. In addition,
-     *    it takes the JSON return data and converts it to an array of objects
-     *    already cast to a class corresponding to our Resource classes.
-     * 
-     * I'm not usually a fan of the pseudo-code crap using the 'new $classname()'
-     *    sort of stuff but it works quite nicely here..
-     * 
-     * @param type $name - name of the resource we're retrieving
-     * @param type $arguments - filters, etc that we want
-     * 
-     * @return stdClass 
-     */
-    public function __call($name, $arguments)
-    {
-        if(isset($this->sub_resources[$name])) {
-            $target_uri = "https://www.contactually.com/api/v1/$name.json";
-            $myObject = $this->_get($target_uri, $arguments[0]);
-
-            $classname = 'Services_Contactually_'.$this->sub_resources[$name];
-
-            $dataSet = $myObject->$name;
-            foreach($dataSet as $key => $values) {
-                $newObject = new $classname();
-                $dataSet[$key] = $newObject->bind($values);
-            }
-            $myObject->$name = $dataSet;
-
-            return $myObject;
-        } else {
-            echo "nope, didn't work";
-            throw new Exception("Method not found", 405);
-        }
     }
 }
--- Services/Contactually/Base.php
+++ Services/Contactually/Base.php
@@ -2,6 +2,13 @@
 
 abstract class Services_Contactually_Base
 {
+    protected $service = null;
+    
+    public function __construct(Services_Contactually $service)
+    {
+        $this->service = $service;
+    }
+
     public function bind($properties)
     {
         foreach($properties as $property => $value) {
@@ -9,5 +16,39 @@ abstract class Services_Contactually_Base
         }
 
         return $this;
+    }
+
+    /**
+     * This handles all the GET index actions for all of the resources. In addition,
+     *    it takes the JSON return data and converts it to an array of objects
+     *    already cast to a class corresponding to our Resource classes.
+     * 
+     * I'm not usually a fan of the pseudo-code crap using the 'new $classname()'
+     *    sort of stuff but it works quite nicely here..
+     * 
+     * @param type $name - name of the resource we're retrieving
+     * @param type $arguments - filters, etc that we want
+     * 
+     * @return stdClass 
+     */
+    public function __call($name, $arguments)
+    {
+        switch($name) {
+            case 'index':
+                $myObject = $this->service->get($this->index, $arguments[0]);
+                $dataSet = $myObject->{$this->name};
+
+                foreach($dataSet as $key => $values) {
+                    $newObject = clone $this;
+                    $dataSet[$key] = $newObject->bind($values);
+                }
+                //$myObject->{$this->name} = $dataSet;
+                
+                return $dataSet;
+                break;
+            default:
+                echo "nope, didn't work";
+                throw new Exception("Method not found", 405);
+        }
     }
 }
