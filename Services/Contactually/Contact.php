84c0d665341356cdc66095ba8c942a501d29fa9b
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-04 02:15:29 -0500

    setting up how the contacts class would work

--- Services/Contactually/Contact.php
+++ Services/Contactually/Contact.php
@@ -2,5 +2,6 @@
 
 class Services_Contactually_Contact extends Services_Contactually_Base
 {
-
+    protected $name  = 'contacts';
+    protected $index = 'https://www.contactually.com/api/v1/contacts.json';
 }
