be0ff688eda5ac6729d4ce483837689b6178b633
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-06 02:31:59 -0500

    updating the paths for the actions;

--- Services/Contactually/Task.php
+++ Services/Contactually/Task.php
@@ -2,5 +2,7 @@
 
 class Services_Contactually_Task extends Services_Contactually_Base
 {
-
+    protected $name  = 'tasks';
+    protected $index = 'https://www.contactually.com/api/v1/tasks.json';
+    protected $show  = 'https://www.contactually.com/api/v1/tasks/<id>.json';
 }
--- Services/Contactually/User.php
+++ Services/Contactually/User.php
@@ -3,4 +3,5 @@
 class Services_Contactually_User extends Services_Contactually_Base
 {
     //TODO: no freakin' clue what methods are implemented on this Resource
+    protected $show  = 'https://www.contactually.com/api/v1/current.json';
 }
