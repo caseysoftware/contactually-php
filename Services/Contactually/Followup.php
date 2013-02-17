f419509ad4ccc7b55f9c53cd70126d00b16986de
D Keith Casey Jr <keith@caseysoftware.com>
2013-02-16 22:43:28 -0600

    added the Followup properties, not documented in the API docs

--- Services/Contactually/Followup.php
+++ Services/Contactually/Followup.php
@@ -2,6 +2,17 @@
 
 class Services_Contactually_Followup extends Services_Contactually_Base
 {
+    public $id = '';
+    public $title = '';
+    public $due_date = '';
+    public $completed_at = '';
+    public $deleted_at = '';
+    public $is_follow_up = '';
+    public $last_contacted = '';
+    public $parent_contact_id = '';
+    public $ignored = '';
+    public $completed_via = '';
+    public $reason = '';
     public function show()
     {
         throw new Services_Contactually_Exception_NotImplemented("This method is not implemented");
