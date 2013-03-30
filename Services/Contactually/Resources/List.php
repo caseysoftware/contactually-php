4b7183e5e3298a4fd64789e872fed5557b910ae1
D Keith Casey Jr <keith@caseysoftware.com>
2013-03-30 12:19:23 -0500

    implemented pagination at the List object level so all the resources inherit it;

--- Services/Contactually/Resources/List.php
+++ Services/Contactually/Resources/List.php
@@ -5,21 +5,73 @@ abstract class Services_Contactually_Resources_List
 {
     protected $client = null;
     protected $_index = 0;
+    protected $_total = 1;
+    protected $_page_count = 1;
+
+    public $page = 1;
+    public $limit = 100;
 
     public function __construct(Services_Contactually $client)
     {
         $this->client = $client;
     }
 
-    public function index()
+    /**
+     * This method gets a specified page of results.
+     *
+     * The API errors if the page < 1 or the limit > 100, so we validate those
+     *   here. I'm not sure if that's the best approach but it works.
+     *
+     * @param type $page
+     * @param type $limit
+     * @return Services_Contactually_Resources_List 
+     */
+    public function index($page = 1, $limit = 100)
     {
-        $this->client->get($this->_index_uri);
+        $this->page = max($page, 1);
+        $this->limit = min($limit, 100);
+
+        $params = array('page' => $this->page, 'limit' => $this->limit);
+        $this->client->get($this->_index_uri, $params);
 
         $object = $this->client->response_obj;
         $this->_obj  = $object->{$this->_data};
+
         $this->count = $object->count;
+        $this->_total = $object->total_count;
+        $this->_page_count = ceil($this->_total / $this->limit);
 
         return $this;
+    }
+
+    public function getTotalRecords()
+    {
+        return $this->_total;
+    }
+
+    public function getPageCount()
+    {
+        return $this->_page_count;
+    }
+
+    public function getNextPage()
+    {
+        $this->page++;
+        $this->page = min($this->page, $this->_page_count);
+
+        return $this->index($this->page, $this->limit);
+    }
+
+    public function getPreviousPage()
+    {
+        $this->page--;
+        $this->page = max($this->page, 1);
+
+        return $this->index($this->page, $this->limit);
+    }
+    public function hasMorePages()
+    {
+        return ($this->page < $this->_page_count);
     }
 
     protected function _getObject($index = 0)
