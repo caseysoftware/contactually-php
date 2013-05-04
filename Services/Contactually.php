b477a0be4612b7b01ac724a3fca4079d658a629d
D Keith Casey Jr <keith@caseysoftware.com>
2013-05-04 09:48:38 -0500

    deprecated the username/password method because it's a screwy cookie-based system that makes things more complicated than necessary

--- Services/Contactually.php
+++ Services/Contactually.php
@@ -59,10 +59,10 @@ class Services_Contactually extends Services_Contactually_Resources_Base
             //do nothing, move along
             $this->_api_key = $params['api_key'];
         } elseif (isset($params['email']) && isset($params['password'])) {
-            foreach($params as $param => $value) {
-                unset($params[$param]);
-                $params["user[$param]"] = $value;
-            }
+            trigger_error("The usernmae/password authentication method has been deprecated.", E_USER_NOTICE);
+
+            $params["user['email]"] = $params['email'];
+            $params["user['password]"] = $params['password'];
             return $this->_authenticate($params);
         } else {
             throw new Services_Contactually_Exceptions_Authentication(
--- examples/authenticate.php
+++ examples/authenticate.php
@@ -4,19 +4,6 @@ include_once '../creds.php';
 include_once '../Services/Contactually.php';
 
 /**
- * This implementation authenticates immediately upon instantiation instead of
- *    waiting for the first request. This also requires edit access to a locally
- *    stored cookie jar. I'm not a huge fan.
- */
-$client = new Services_Contactually(
-            array('email' => $email, 'password' => $password)
-        );
-
-echo $client->response_code;
-echo "\n";
-unset($client);
-
-/**
  * This implementation doesn't authenticate until the request is actually made.
  *    I believe it is better fundamentally because it doesn't retain a state
  *    between requests.
