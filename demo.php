18f86f80fb029234b60450f1ad8866dce987fd42
Keith Casey <keith@caseysoftware.com>
2014-07-21 12:24:16 -0500

    tweak to the authentication

--- demo.php
+++ demo.php
@@ -4,9 +4,7 @@ include_once 'creds.php';
 include_once 'Services/Contactually.php';
 
 echo '<pre>';
-$service = new Services_Contactually(
-            array('email' => $email, 'password' => $password)
-        );
+$service = new Services_Contactually(array('api_key' => $apiKey));
 
 $results = $service->contacts(array('limit' => 10));
 print_r($results);
