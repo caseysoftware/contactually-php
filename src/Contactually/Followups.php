00333558b81325f1378b086d3be3a55fa8161fbd
Keith Casey <keith@caseysoftware.com>
2014-12-05 23:57:21 -0600

    wired the followup->list() method

--- src/Contactually/Followups.php
+++ src/Contactually/Followups.php
@@ -6,4 +6,13 @@ class Followups extends \Contactually\Common\Resource
 {
     protected $resource = 'followups';
     protected $dataname = 'today';
+
+    public function list_($parameters = array())
+    {
+        $results = $this->client->get($this->resource . '/list.json', $parameters);
+
+        $this->data = $results[$this->dataname];
+
+        return $this;
+    }
 }
