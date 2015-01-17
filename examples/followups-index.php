8e7a236d93647346581bbcf2c24a2166e78179bf
Keith Casey <keith@caseysoftware.com>
2015-01-16 23:12:32 -0600

    wired the followups to work as expected

--- examples/followups-index.php
+++ examples/followups-index.php
@@ -8,5 +8,6 @@ $client = new \Contactually\Client($apikey);
 $followups = $client->followups->index();
 
 foreach($followups as $followup) {
-    print_r($followup);
+    echo $followup->title . "\n";
+    echo $followup->due_date . "\n";
 }
