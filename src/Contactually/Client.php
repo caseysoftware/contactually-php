63d5aac4dc03d416640a235ac9a340b2e31242b7
Keith Casey <keith@caseysoftware.com>
2015-01-23 23:25:31 -0600

    wiring the api key for creating new resources

--- src/Contactually/Client.php
+++ src/Contactually/Client.php
@@ -72,6 +72,7 @@ class Client
     {
         /** @var $request \Guzzle\Http\Message\Request */
         $request = $this->client->post($uri, array(), '', array('exceptions' => false));
+        $params['api_key'] = $this->apikey;
         foreach($params as $key => $value) {
             $request->setPostField($key, $value);
         }
