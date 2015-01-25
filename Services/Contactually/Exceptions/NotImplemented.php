b415d9b2863279887d25b8e3d00efb65907634df
Keith Casey <keith@caseysoftware.com>
2015-01-24 22:37:54 -0600

    removing the old library structure

--- Services/Contactually/Exceptions/NotImplemented.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Services_Contactually_Exceptions_NotImplemented extends Exception
-{
-    
-}
--- Services/Contactually/Interfaces/Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface Services_Contactually_Interfaces_Index
-{
-    public function index();
-}
