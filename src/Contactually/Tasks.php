348c946d068d1ee6ed0d61780e5d5414f4205038
Keith Casey <keith@caseysoftware.com>
2015-02-25 10:39:10 -0600

    shifted the ignore/snooze methods
    Turns out the docs are out of date and the Tasks resource has these
    methods, not the Contacts resource

--- src/Contactually/Tasks.php
+++ src/Contactually/Tasks.php
@@ -6,4 +6,22 @@ class Tasks extends \Contactually\Resources\Base
 {
     protected $resource = 'tasks';
     protected $dataname = 'tasks';
+
+    public function ignore($temporary = false)
+    {
+        $parameters = array('temp' => $temporary, 'task_id' => $task_id);
+
+        $results = $this->client->post($this->resource . '/' . $this->id . '/ignore.json', $parameters);
+
+        return $results;
+    }
+
+    public function snooze($days = 7)
+    {
+        $parameters = array('days' => $days, 'task_id' => $task_id);
+
+        $results = $this->client->post($this->resource . '/' . $this->id . '/snooze.json', $parameters);
+
+        return $results;
+    }
 }
